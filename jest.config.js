d363241f61182a3f418508cd2ae81d28a96ea00e
elegardo <elegardo@gmail.com>
2019-11-13 13:10:04 -0300

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 /* eslint-disable no-undef */
 module.exports = {
+    verbose: true,
     roots: ['<rootDir>/src'],
     transform: {
       '^.+\\.ts?$': 'ts-jest',
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    collectCoverageFrom: [
+      "**/*.{ts,js}",
+      "!**/node_modules/**",
+    ],
+    coverageThreshold: {
+      global: {
+        branches: 0,
+        functions: 0,
+        lines: 0,
+        statements: 0
+      }
+    }
   }
