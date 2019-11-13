6bab252b4e92e349e85a56530155a9f5bb7ef8c5
elegardo <elegardo@gmail.com>
2019-11-13 14:58:41 -0300

    add testing with sinon

--- src/services/__tests__/UserServiceImpl.spec.ts
+++ src/services/__tests__/UserServiceImpl.spec.ts
@@ -1,15 +1,31 @@
+/* eslint-disable */
 import { User } from '../../domain/User'
 import { UserServiceImpl } from '../UserServiceImpl';
-
-const service: UserServiceImpl = new UserServiceImpl();
+import sinon from 'sinon';
 
 describe('UserServiceImpl', () => {
-    
+
+    let service: UserServiceImpl;
+    let sandbox: sinon.SinonSandbox;
+
+    beforeEach(() => {
+        service = new UserServiceImpl();
+        sandbox = sinon.createSandbox();
+    })
+    afterEach(() => { 
+        sandbox.restore();
+    })
+
     it('obtainAllUsers should deliver all user.', () => {
-        const expectUsers: User[] = [{ id: 1, name: '', email: '' }];
+        const expectUsers: Promise<User[]> = Promise
+                            .resolve([
+                                { id: 1, name: 'name1', email: '' }
+                            ]);
 
+        sinon.stub(UserServiceImpl.prototype, <any>'obtainAllUsers')
+                .returns(Promise.resolve(expectUsers));
         const users: Promise<User[]> = service.obtainAllUsers();
-
-        expect(Promise.resolve(expectUsers)).toEqual(users);
+        
+        expect(expectUsers).toEqual(users);
     });
 });
