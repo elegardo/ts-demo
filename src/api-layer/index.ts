56e932f9189f48e0ad272e55e855e0afa1f86010
elegardo <elegardo@gmail.com>
2019-11-17 20:40:26 -0300

    refactoring...

--- src/api-layer/index.ts
+++ src/api-layer/index.ts
@@ -1,6 +1,6 @@
 import DatabaseClient from './utils/DatabaseClient';
 import { Logger } from './utils/Logger';
-import HandleError from './handle.error';
+import HandleError from './utils/handle.error';
 import UserRoutes from './routes/user.routes';
 
 export { DatabaseClient, Logger, HandleError, UserRoutes };
