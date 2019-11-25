cc60ad9388d54d0b63ee4af52a722489b06a54de
elegardo <elegardo@gmail.com>
2019-11-25 14:12:07 -0300

    add testing

--- src/useCases/cases/__tests__/ObtainAllUsersImpl.spec.ts
+++ src/useCases/cases/__tests__/ObtainAllUsersImpl.spec.ts
@@ -1,28 +1,48 @@
-/* eslint-disable */
-import { User } from '../../entity/User';
 import { ObtainAllUsersImpl } from '../ObtainAllUsersImpl';
-import { UserRepositoryImpl } from '../../../dataAccess/repository/UserRepositoryImpl';
-import sinon from 'sinon';
+import { User } from '../../entity/User';
 
 describe('ObtainAllUsersImpl', () => {
-    let service: ObtainAllUsersImpl;
-    let sandbox: sinon.SinonSandbox;
+    let MockRepository: jest.Mock;
 
     beforeEach(() => {
-        sandbox = sinon.createSandbox();
+        MockRepository = jest.fn(() => ({
+            findAll: jest.fn(),
+            findBy: jest.fn(),
+        }));
     });
-    afterEach(() => {
-        sandbox.restore();
+
+    it('obtainAllUsers.findAll is called once.', () => {
+        const mock = new MockRepository();
+        const instance = new ObtainAllUsersImpl(mock);
+
+        instance.obtainAllUsers();
+
+        expect(mock.findAll).toHaveBeenCalled();
     });
 
     it('obtainAllUsers should deliver all user.', () => {
-        var myServiceMock: UserRepositoryImpl = <UserRepositoryImpl>(<any>sinon.mock(UserRepositoryImpl));
-        service = new ObtainAllUsersImpl(myServiceMock);
-        const expectUsers: Promise<User[]> = Promise.resolve([{ id: 1, name: 'name1', email: '' }]);
+        const mock = new MockRepository();
+        const expectUsers: User[] = [{ id: 1, name: 'name1', email: '' }];
+        mock.findAll.mockReturnValue(Promise.resolve(expectUsers));
 
-        sinon.stub(ObtainAllUsersImpl.prototype, <any>'obtainAllUsers').returns(Promise.resolve(expectUsers));
-        const users: Promise<User[]> = service.obtainAllUsers();
+        const instance = new ObtainAllUsersImpl(mock);
 
-        expect(expectUsers).toEqual(users);
+        return instance.obtainAllUsers().then(res => {
+            expect(res).toEqual(expectUsers);
+        });
+    });
+
+    it('obtainAllUsers throw Error when repository throw Error', () => {
+        const mock = new MockRepository();
+        const expectError = new Error('Error repository');
+        mock.findAll.mockImplementation(() => {
+            throw expectError;
+        });
+
+        const instance = new ObtainAllUsersImpl(mock);
+
+        return instance.obtainAllUsers().catch(error => {
+            expect(error).toEqual(expectError);
+        });
     });
 });
