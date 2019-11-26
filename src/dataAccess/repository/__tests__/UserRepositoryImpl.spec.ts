3f36fcac15fe9711d21876738bb54d81c29b46b3
elegardo <elegardo@gmail.com>
2019-11-25 23:51:40 -0300

    testing

--- src/dataAccess/repository/__tests__/UserRepositoryImpl.spec.ts
+++ src/dataAccess/repository/__tests__/UserRepositoryImpl.spec.ts
@@ -1,5 +1,6 @@
 import { UserRepositoryImpl } from '../UserRepositoryImpl';
 import { UserModel } from '../../model/UserModel';
+import { NotFoundError } from '../../error/NotFoundError';
 
 describe('UserRepositoryImpl', () => {
     let MockDBClient: jest.Mock;
@@ -32,16 +33,48 @@ describe('UserRepositoryImpl', () => {
         });
     });
 
-    it('findAll throw Error when repository throw Error', () => {
+    it('findAll throw Error when quey repository', async () => {
         const mock = new MockDBClient();
-        const expectError = new Error('Error query');
         mock.query.mockImplementation(() => {
-            throw expectError;
+            throw new Error('Error query');
         });
 
         const instance = new UserRepositoryImpl(mock);
 
-        return instance.findAll().catch(error => {
+        await expect(instance.findAll()).rejects.toThrow();
+    });
+
+    it('findBy is called once.', () => {
+        const mock = new MockDBClient();
+        const instance = new UserRepositoryImpl(mock);
+
+        instance.findBy(1);
+
+        expect(mock.query).toHaveBeenCalled();
+    });
+
+    it('findBy with id = 1 return one user with user.id = 1.', () => {
+        const mock = new MockDBClient();
+        const expectQuery: any = { rows: [{ id: 1, name: 'name1', email: 'email1' }] };
+        const expectUser: UserModel = new UserModel(1, 'name1', 'email1');
+        mock.query.mockReturnValue(Promise.resolve(expectQuery));
+
+        const instance = new UserRepositoryImpl(mock);
+
+        return instance.findBy(1).then(res => {
+            expect(res).toEqual(expectUser);
+        });
+    });
+
+    it('findBy throw NotFoundError when user will not exist.', () => {
+        const mock = new MockDBClient();
+        const expectQuery: any = { rows: [] };
+        const expectError = new NotFoundError('Users not exist');
+        mock.query.mockReturnValue(Promise.resolve(expectQuery));
+
+        const instance = new UserRepositoryImpl(mock);
+
+        return instance.findBy(1).catch(error => {
             expect(error).toEqual(expectError);
         });
     });
